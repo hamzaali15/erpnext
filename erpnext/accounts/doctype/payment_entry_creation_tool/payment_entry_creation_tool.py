f8cff61780d68706f4f2208af2402749df2de900
hamzaali15 <malikhamzaali48@gmail.com>
2023-12-23 11:58:59 +0500

    temp changes

--- erpnext/accounts/doctype/payment_entry_creation_tool/payment_entry_creation_tool.py
+++ erpnext/accounts/doctype/payment_entry_creation_tool/payment_entry_creation_tool.py
@@ -36,39 +36,42 @@ class PaymentEntryCreationTool(Document):
 				frappe.msgprint('The File extension should be CSV or XLSX type.', 'Invalid File')
 				return
 			frappe.msgprint("Payment Entry is Creating in the background")
-			frappe.enqueue(create_payment_entries, self=self, queue="long")
+			if len(order_ids_list) <= 10:
+				create_payment_entries(self)
+			else:
+				frappe.enqueue(create_payment_entries, self=self, queue="long")
 			
 		else:
 			frappe.msgprint('The File does not exist.', 'Invalid File')
 			return
 
-	@frappe.whitelist()
-	def update_order_status_after_payment(self):
-		if frappe.db.exists('File', {'file_url': self.sales_invoice_list}):
-			file_doc = frappe.get_doc("File", {"file_url": self.sales_invoice_list})
-			parts = file_doc.get_extension()
-			extension = parts[1]
-			if extension == '.xlsx':
-				try:
-					content = file_doc.get_content()
-					workbook = openpyxl.load_workbook(BytesIO(content))
-					sheet = workbook.active
-					order_ids_list = [str(cell.value) for cell in sheet['A'][1:]]
-				except Exception as e:
-					frappe.msgprint(f"Error reading XLSX file: {str(e)}", 'Invalid File')
-					return
-			elif extension == '.csv':
-				content = file_doc.get_content()
-				order_ids_list = content.strip().split('\n')[1:]
-			else:
-				frappe.msgprint('The File extension should be CSV or XLSX type.', 'Invalid File')
-				return
-			frappe.msgprint("Sales Order status is updating in the background")
-			frappe.enqueue(update_order_status_after_payment, self=self, queue="long")
-			return "Success"
-		else:
-			frappe.msgprint('The File does not exist.', 'Invalid File')
-			return
+	# @frappe.whitelist()
+	# def update_order_status_after_payment(self):
+	# 	if frappe.db.exists('File', {'file_url': self.sales_invoice_list}):
+	# 		file_doc = frappe.get_doc("File", {"file_url": self.sales_invoice_list})
+	# 		parts = file_doc.get_extension()
+	# 		extension = parts[1]
+	# 		if extension == '.xlsx':
+	# 			try:
+	# 				content = file_doc.get_content()
+	# 				workbook = openpyxl.load_workbook(BytesIO(content))
+	# 				sheet = workbook.active
+	# 				order_ids_list = [str(cell.value) for cell in sheet['A'][1:]]
+	# 			except Exception as e:
+	# 				frappe.msgprint(f"Error reading XLSX file: {str(e)}", 'Invalid File')
+	# 				return
+	# 		elif extension == '.csv':
+	# 			content = file_doc.get_content()
+	# 			order_ids_list = content.strip().split('\n')[1:]
+	# 		else:
+	# 			frappe.msgprint('The File extension should be CSV or XLSX type.', 'Invalid File')
+	# 			return
+	# 		frappe.msgprint("Sales Order status is updating in the background")
+	# 		frappe.enqueue(update_order_status_after_payment, self=self, queue="long")
+	# 		return "Success"
+	# 	else:
+	# 		frappe.msgprint('The File does not exist.', 'Invalid File')
+	# 		return
 
 
 def create_payment_entries(self):
@@ -89,87 +92,91 @@ def create_payment_entries(self):
 			content = file_doc.get_content()
 			order_ids_list = content.strip().split('\n')
 	for d in order_ids_list:
-		if frappe.db.exists("Sales Invoice", {"po_no": d, "docstatus": 1, "status": ["in", ["Unpaid", "Overdue"]]}):
-			try:
-				si = frappe.get_doc("Sales Invoice", {"po_no": d}).as_dict()
-				# pe = get_payment_entry("Sales Invoice", si.name)
-				# pe.po_no = d
-				# pe.set_missing_values()
+		if d and d != 'None':
+			if frappe.db.exists("Sales Invoice", {"po_no": d, "docstatus": 1, "status": ["in", ["Unpaid", "Overdue"]]}):
+				try:
+					si = frappe.get_doc("Sales Invoice", {"po_no": d}).as_dict()
+					# pe = get_payment_entry("Sales Invoice", si.name)
+					# pe.po_no = d
+					# pe.set_missing_values()
 
-				# bank_account = pe.paid_to if pe.payment_type == "Receive" else pe.paid_from
-				# bank_account_type = frappe.db.get_value("Account", bank_account, "account_type")
-				# if bank_account_type == "Bank":
-				# 	if not pe.reference_no or not pe.reference_date:
-				# 		pe.reference_no = si.name
-				# 		pe.reference_date = nowdate()
+					# bank_account = pe.paid_to if pe.payment_type == "Receive" else pe.paid_from
+					# bank_account_type = frappe.db.get_value("Account", bank_account, "account_type")
+					# if bank_account_type == "Bank":
+					# 	if not pe.reference_no or not pe.reference_date:
+					# 		pe.reference_no = si.name
+					# 		pe.reference_date = nowdate()
 
-				# pe.insert(ignore_permissions=True)
-				# pe.save(ignore_permissions=True)
-				# pe.submit()
-				# frappe.db.commit()
+					# pe.insert(ignore_permissions=True)
+					# pe.save(ignore_permissions=True)
+					# pe.submit()
+					# frappe.db.commit()
 
-				# sales_order = frappe.get_doc("Sales Order", {"docstatus": 1, "woocommerce_id": d}).as_dict()
-				pe = change_status(si, "payment_entry")
+					# sales_order = frappe.get_doc("Sales Order", {"docstatus": 1, "woocommerce_id": d}).as_dict()
+					pe = change_status([si], "payment_entry")
 
-				#updating sales order status after payment entry creation
-				# so = frappe.get_doc("Sales Order", {"docstatus": 1, "woocommerce_id": d})
-				# so.woocommerce_status = "delivered"
-				# so.status = "Delivered"
-				# so.db_update()
-				# frappe.db.commit()
+					#updating sales order status after payment entry creation
+					# so = frappe.get_doc("Sales Order", {"docstatus": 1, "woocommerce_id": d})
+					# so.woocommerce_status = "delivered"
+					# so.status = "Delivered"
+					# so.db_update()
+					# frappe.db.commit()
 
-				self.append("invoices_detail", {
-					"order_id": d,
-					"status": "Success",
-					"description": "Payment Entry Created Successfully"
-				})
-				self.save()
-			except Exception as e:
+					self.append("invoices_detail", {
+						"order_id": d,
+						"status": "Success",
+						"description": "Payment Entry Created Successfully"
+					})
+					self.save()
+					frappe.db.commit()
+				except Exception as e:
+					self.append("invoices_detail", {
+						"order_id": d,
+						"status": "Failed",
+						"description": e  #"Payment Entry Creation Failed"
+					})
+					self.save()
+					frappe.db.commit()
+			else:
 				self.append("invoices_detail", {
 					"order_id": d,
 					"status": "Failed",
-					"description": e  #"Payment Entry Creation Failed"
+					"description": "Sales Invoice is Paid/Cancelled or does not Exist"
 				})
-				self.save()
-		else:
-			self.append("invoices_detail", {
-				"order_id": d,
-				"status": "Failed",
-				"description": "Sales Invoice is Paid/Cancelled or does not Exist"
-			})
+	# self.db_set('completed', 1)
 	self.completed = 1
 	self.save()
 	frappe.db.commit()
 
 
 
-def update_order_status_after_payment(self):
-	if frappe.db.exists('File', {'file_url': self.sales_invoice_list}):
-		file_doc = frappe.get_doc("File", {"file_url": self.sales_invoice_list})
-		parts = file_doc.get_extension()
-		extension = parts[1]
-		if extension == '.xlsx':
-			try:
-				content = file_doc.get_content()
-				workbook = openpyxl.load_workbook(BytesIO(content))
-				sheet = workbook.active
-				order_ids_list = [str(cell.value) for cell in sheet['A'][1:]]
-			except Exception as e:
-				frappe.msgprint(f"Error reading XLSX file: {str(e)}", 'Invalid File')
-				return
-		elif extension == '.csv':
-			content = file_doc.get_content()
-			order_ids_list = content.strip().split('\n')
+# def update_order_status_after_payment(self):
+# 	if frappe.db.exists('File', {'file_url': self.sales_invoice_list}):
+# 		file_doc = frappe.get_doc("File", {"file_url": self.sales_invoice_list})
+# 		parts = file_doc.get_extension()
+# 		extension = parts[1]
+# 		if extension == '.xlsx':
+# 			try:
+# 				content = file_doc.get_content()
+# 				workbook = openpyxl.load_workbook(BytesIO(content))
+# 				sheet = workbook.active
+# 				order_ids_list = [str(cell.value) for cell in sheet['A'][1:]]
+# 			except Exception as e:
+# 				frappe.msgprint(f"Error reading XLSX file: {str(e)}", 'Invalid File')
+# 				return
+# 		elif extension == '.csv':
+# 			content = file_doc.get_content()
+# 			order_ids_list = content.strip().split('\n')
 
-		for d in order_ids_list:
-			if frappe.db.exists("Sales Order", {"woocommerce_id": d, "docstatus": 1}):
-				if frappe.db.exists("Sales Invoice", {"po_no": d, "docstatus": 1}):
-					if frappe.db.exists("Payment Entry", {"po_no": d, "docstatus": 1}):
+# 		for d in order_ids_list:
+# 			if frappe.db.exists("Sales Order", {"woocommerce_id": d, "docstatus": 1}):
+# 				if frappe.db.exists("Sales Invoice", {"po_no": d, "docstatus": 1}):
+# 					if frappe.db.exists("Payment Entry", {"po_no": d, "docstatus": 1}):
 
-						url = 'https://baahy.com/wp-json/api/gibran_user/erp_sync_status'
-						status = 'delivered'
-						myObj = {'id': d, 'status': status}
-						json_data = requests.post(url, json=myObj, headers={"Content-Type":"application/json"})
+# 						url = 'https://baahy.com/wp-json/api/gibran_user/erp_sync_status'
+# 						status = 'delivered'
+# 						myObj = {'id': d, 'status': status}
+# 						json_data = requests.post(url, json=myObj, headers={"Content-Type":"application/json"})
 
 
 # def update_order_status_after_payment(self):
